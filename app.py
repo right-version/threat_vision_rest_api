5c4577b142229f375f61fb5d57b830f759f626ca
vpvpvpvp <vktrpnzrv@gmail.com>
2020-08-22 07:59:56 +0300

    Model Inference

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask, jsonify, render_template, request
 from flask_cors import CORS, cross_origin
+import pandas as pd
+import torch
+from torch.utils.data import DataLoader
+from model.autoencoder import Autoencoder
+from data.traffic_data_generator import TrafficDataset
 
 DEBUG = True
 
@@ -10,6 +15,35 @@ app.config.from_object(__name__)
 # Enable CORS
 CORS(app, resources={r"/api/*": {"origins": "*"}})
 
+checkpoint = torch.load("./model/model_latest.pth")
+model = Autoencoder()
+model.load_state_dict(checkpoint['model_state_dict'])
+model.eval()
+
+
+def predict(test_loader: DataLoader) -> dict:
+    """
+    Evaluate model on the received dataset.
+
+    Args:
+        test_loader: received dataset
+
+    Returns:
+        dict with the probabilities for each row in the test_loader
+    """
+    probs = dict()
+
+    criterion = torch.nn.MSELoss()
+    with torch.no_grad():
+        for i, batch in enumerate(test_loader):
+
+            outputs = model(batch)
+
+            test_loss = criterion(outputs, batch)
+
+            probs[str(i)] = str(test_loss.item())
+    return probs
+
 
 # Sanity check route
 @app.route('/ping', methods=['GET'])
@@ -18,12 +52,17 @@ def ping():
     return jsonify('pong!')
 
 
-@app.route('/pong', methods=['POST'])
+@app.route('/predict', methods=['POST'])
 @cross_origin()
 def pong():
     file = request.files['file']
-    read = file.read()
-    return read
+    data = pd.read_csv(file)
+    dataset_test = TrafficDataset(data)
+    test_loader = DataLoader(dataset_test, batch_size=1)
+
+    probs = predict(test_loader)
+
+    return jsonify(probs)
 
 
 @app.route('/')
